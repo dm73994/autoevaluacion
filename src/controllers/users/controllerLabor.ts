4c1c1f6e11bc3460cdeabb40b2544a12f21c257a
jdacamacho <jdacamacho@unicauca.edu.co>
2023-06-06 16:33:17 -0500

    asd

--- src/controllers/users/controllerLabor.ts
+++ src/controllers/users/controllerLabor.ts
@@ -3,7 +3,7 @@ import { connection } from '../../database/db';
 export const createLabor = (req, res) => {
     try{
         const {labor_code,labor_name,type_labor_code,labor_hours}: any = req.body;
-        console.log('CONTROLLER CREATE LABOR')
+        console.log('CONTROLLER CREATE LABOR');
         connection.query('INSERT INTO labor SET ?', {labor_code: labor_code ,labor_name: labor_name,type_labor_code: type_labor_code,labor_hours: labor_hours},(err, result)=>{
             if(err){
                 console.log(err);
@@ -13,6 +13,41 @@ export const createLabor = (req, res) => {
             }
         })
     }catch(err){
+        console.log(err);
+    }
+}
+
+export const updateLabor = (req, res) => {
+
+    try{
+        const {labor_code,labor_name,type_labor_code,labor_hours}: any = req.body;
+        console.log('CONTROLLER UPDATE LABOR');
+        connection.query('UPDATE labor SET ? WHERE labor_code = ?',[{labor_name: labor_name,type_labor_code: type_labor_code,labor_hours: labor_hours},labor_code],(err, result)=>{
+            if(err){
+                console.log(err);
+            }
+            else{
+                console.log('labor actualizada');
+            }
+        })
+    }catch(err){
+        console.log(err);
+    }
+}
+
+export const disableLabor = (req, res) => {
+
+    try {
+        const { labor_code, activo } = req.body;
+        console.log('CONTROLLER UPDATE LABOR');
+        connection.query('UPDATE labor SET activo = ? WHERE labor_code = ?', [activo, labor_code], (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log('Labor deshabilitada');
+            }
+        });
+    } catch (err) {
         console.log(err);
     }
 }
--- src/routes/labores/laboresRoute.ts
+++ src/routes/labores/laboresRoute.ts
@@ -1,7 +1,9 @@
 import express from 'express';
-import { createLabor } from '../../controllers/users/controllerLabor';
+import { createLabor, disableLabor, updateLabor } from '../../controllers/users/controllerLabor';
 
 export const laborRouter = express.Router();
 
 laborRouter.route('/createLabor')
     .post(createLabor)
+    .put(updateLabor)
+    .put(disableLabor)
