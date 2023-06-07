f93611812accfacca965aef1e16cbb8a3347296b
CMLdev-cmesa <cmesa@cmlexports.com>
2023-06-07 17:33:32 -0500

    sanitizacion

--- src/routes/labores/laboresRoute.ts
+++ src/routes/labores/laboresRoute.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 import { createLabor, updateLabor } from '../../controllers/labor/controllerLabor';
+import { sanitizeLaborData } from '../../middleware/sanitize';
 
 export const laborRouter = express.Router();
 
 laborRouter.route('/')
     .post(createLabor)
-    .put(updateLabor)
+    .put(sanitizeLaborData, updateLabor)
