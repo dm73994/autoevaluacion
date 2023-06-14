680e9240ba98453c0056dda0268cdc033166b236
jdacamacho <jdacamacho@unicauca.edu.co>
2023-06-14 16:58:17 -0500

    ultimaActualiacion miercoles

--- src/routes/autoevaluacion/autoevaluacionRoute.ts
+++ src/routes/autoevaluacion/autoevaluacionRoute.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createAutoevaluacion } from '../../controllers/users/controllerAutoevaluacion';
+import { createAutoevaluacion } from '../../controllers/autoevaluacion/controllerAutoevaluacion';
 
 export const autoEvaluationRouter = express.Router();
 
--- src/routes/periodoAcademico/periodRoute.ts
+++ src/routes/periodoAcademico/periodRoute.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createPeriodoAcademico } from '../../controllers/users/controllerPeriodoAcademico';
+import { createPeriodoAcademico } from '../../controllers/periodoAcademico/controllerPeriodoAcademico';
 
 export const periodoRouter = express.Router();
 
